881dee68ee541b6405b17ed5887015ea83526de2
VadimKutovoi <sir.kutovoi@yandex.ru>
2021-03-20 14:36:01 +0300

    fixed bugs in polynomial

--- polynomial.py
+++ polynomial.py
@@ -13,25 +13,63 @@ class Polynomial:
         elif type(coefs) is Polynomial:
             self.coefs = coefs.coefs.copy()
             self.max_pow = coefs.max_pow
+        elif type(coefs) in (int, float):
+            self.coefs = [coefs]
         else:
-            raise TypeError("coefs should be list or Polynomial")
+            raise TypeError("coefs should be list, tuple, Polynomial or single int, float")
+
+    @staticmethod
+    def __coef_to_term(number):
+        if number > 0:
+            return f'+{number}'
+        elif number < 0:
+            return str(number)
+        else:
+            return None
+
+    @staticmethod
+    def __get_sign(number):
+        if number > 0:
+            return f''
+        elif number < 0:
+            return f'-'
+        else:
+            return None
 
     def __str__(self):
         pol_as_str = ''
-        curr_pow = self.max_pow
-        if self.max_pow > 0:
-            for coef in self.coefs[0:self.max_pow - 1]:
-                if not coef == 0:
-                    pol_as_str += str(coef) + f'x^{curr_pow} + '
+        is_first_coef = True
+        if self.max_pow == 0:
+            return str(self.coefs[0])
+        else:
+            curr_pow = self.max_pow
+            for coef in self.coefs:
+                if coef != 0:
+                    if not is_first_coef:
+                        if coef == -1 and curr_pow != 0:
+                            pol_as_str += '-'
+                        elif coef == 1 and curr_pow != 0:
+                            pol_as_str += '+'
+                        else:
+                            pol_as_str += self.__coef_to_term(coef)
+                    else:
+                        pol_as_str = str(self.__get_sign(coef)) if coef == 1 or coef == -1 \
+                                                                else str(coef)
+                        is_first_coef = False
+                    if curr_pow == 1:
+                        pol_as_str += f'x'
+                    elif curr_pow != 0:
+                        pol_as_str += f'x^{curr_pow}'
                 curr_pow -= 1
-            if (self.coefs[-2] != 0):
-                pol_as_str += str(self.coefs[-2]) + f'x + '
-            if (self.coefs[-1] != 0):
-                pol_as_str += str(self.coefs[-1])
         return pol_as_str
 
     def __repr__(self):
         return f"Polynomial({self.coefs})"
+
+    def __setattr__(self, key, value):
+        self.__dict__[key] = value
+        if key == "coefs":
+            self.max_pow = len(self.coefs) - 1
 
     def __add__(self, other):
         if type(other) in (int, float):
@@ -54,10 +92,21 @@ class Polynomial:
         return Polynomial(coefs)
 
     def __sub__(self, other):
-        coefs = other.coefs.copy()
+        if type(other) == Polynomial:
+            coefs = other.coefs.copy()
+        elif type(other) in (float, int):
+            coefs = [other]
+        else:
+            raise TypeError(f"can't sub {type(self)} with {type(other)}")
         for i in range(0, len(coefs)):
             coefs[i] = -coefs[i]
         return self.__add__(Polynomial(coefs))
+
+    def __rsub__(self, other):
+        tmp = Polynomial(self)
+        for i in range(0, len(tmp.coefs)):
+            tmp.coefs[i] = -tmp.coefs[i]
+        return tmp.__add__(other)
 
     def __mul__(self, other):
         if type(other) in (int, float):
@@ -77,11 +126,12 @@ class Polynomial:
         if type(other) in (int, float):
             if self.max_pow == 0:
                 return self.coefs[-1] == other
+            else:
+                return False
         elif type(other) is Polynomial:
             return self.coefs == other.coefs
         else:
             raise TypeError(f"can't compare {type(self)} with {type(other)}")
 
     __radd__ = __add__
-    __rsub__ = __sub__
     __rmul__ = __mul__ 
